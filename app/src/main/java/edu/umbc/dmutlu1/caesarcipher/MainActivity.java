e16105c913b6d6a306ba7fe641707d03f98a04bf
dmutlu <devin.mutlu+github@gmail.com>
2017-11-13 01:47:47 -0500

    Fixed RecyclerView scrolling interaction with toolbar.

--- app/src/main/java/edu/umbc/dmutlu1/caesarcipher/MainActivity.java
+++ app/src/main/java/edu/umbc/dmutlu1/caesarcipher/MainActivity.java
@@ -40,7 +40,6 @@ public class MainActivity extends AppCompatActivity
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
         mRecyclerView.setHasFixedSize(true);
-        mRecyclerView.setNestedScrollingEnabled(true);
 
         // use a linear layout manager
         mLayoutManager = new LinearLayoutManager(this);
