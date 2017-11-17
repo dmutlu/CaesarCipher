d54345fa35b6ec9ac14553dffc9560041ec6f7ec
dmutlu <devin.mutlu+github@gmail.com>
2017-11-17 10:09:24 -0500

    Added basic splash screen.
    Signed-off-by: dmutlu <devin.mutlu+github@gmail.com>

---
+++ app/src/main/java/edu/umbc/dmutlu1/caesarcipher/SplashActivity.java
@@ -0,0 +1,34 @@
+package edu.umbc.dmutlu1.caesarcipher;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        TimerTask task = new TimerTask()
+        {
+            @Override
+            public void run()
+            {
+                // Start home activity
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+
+                // close splash activity
+                finish();
+            }
+        };
+
+        Timer launch = new Timer();
+        launch.schedule(task,1000);
+    }
+}
