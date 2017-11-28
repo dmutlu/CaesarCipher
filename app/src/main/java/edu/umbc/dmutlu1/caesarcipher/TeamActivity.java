ce43b8e97c0790648935e46c0bed5c125227b815
Devin Mutlu <dmutlu@users.noreply.github.com>
2017-11-28 16:24:39 -0500

    Touch-ups and comments.
    Signed-off-by: dmutlu <devin.mutlu+github@gmail.com>

--- app/src/main/java/edu/umbc/dmutlu1/caesarcipher/TeamActivity.java
+++ app/src/main/java/edu/umbc/dmutlu1/caesarcipher/TeamActivity.java
@@ -1,6 +1,7 @@
 package edu.umbc.dmutlu1.caesarcipher;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.animation.Animation;
@@ -12,6 +13,7 @@ public class TeamActivity extends AppCompatActivity
 {
     private int eggCounter = 0;
     private int eggDisplay = 2;
+    private Toast toast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -19,32 +21,46 @@ public class TeamActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_team);
 
+        Context context = getApplicationContext();
         Animation animation = AnimationUtils.loadAnimation(this, R.anim.img_rotate);
-
         ImageView imageView = findViewById(R.id.imageTeam);
+
         imageView.setOnClickListener(view -> {
 
+            //Check to see if animation is already running.
             if (!animation.hasStarted() || animation.hasEnded())
             {
                 imageView.startAnimation(animation);
                 eggCounter++;
 
+                //When to show easter egg hint toasts.
                 if (eggCounter >= 4 && eggCounter < 6)
                 {
-                    Context context = getApplicationContext();
-                    CharSequence text = "You are now " + eggDisplay + " presses from revealing the easter egg.";
+                    Resources res = getResources();
+
+                    //Grab our plurals for the countdown hints.
+                    String text = res.getQuantityString(R.plurals.easter_toast, eggDisplay, eggDisplay);
                     int duration = Toast.LENGTH_SHORT;
 
-                    Toast toast = Toast.makeText(context, text, duration);
+                    toast = Toast.makeText(context, text, duration);
                     toast.show();
 
                     eggDisplay--;
                 }
             }
 
+            //When to change the original image to the easter egg image.
             if (eggCounter == 6)
             {
+                //Set to easter egg image.
                 imageView.setImageResource(R.drawable.caesar_salad);
+
+                //Acknowledgement toast.
+                String text = getString(R.string.toast_reveal);
+                int duration = Toast.LENGTH_SHORT;
+
+                toast = Toast.makeText(context, text, duration);
+                toast.show();
             }
         });
     }
