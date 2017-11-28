2c9df5b2701a01b36dcf12b3c9e2c46fce91406d
Devin Mutlu <dmutlu@users.noreply.github.com>
2017-11-28 15:59:31 -0500

    Added easter egg.
    Signed-off-by: dmutlu <devin.mutlu+github@gmail.com>

--- app/src/main/java/edu/umbc/dmutlu1/caesarcipher/TeamActivity.java
+++ app/src/main/java/edu/umbc/dmutlu1/caesarcipher/TeamActivity.java
@@ -1,13 +1,17 @@
 package edu.umbc.dmutlu1.caesarcipher;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class TeamActivity extends AppCompatActivity
 {
+    private int eggCounter = 0;
+    private int eggDisplay = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -23,6 +27,24 @@ public class TeamActivity extends AppCompatActivity
             if (!animation.hasStarted() || animation.hasEnded())
             {
                 imageView.startAnimation(animation);
+                eggCounter++;
+
+                if (eggCounter >= 4 && eggCounter < 6)
+                {
+                    Context context = getApplicationContext();
+                    CharSequence text = "You are now " + eggDisplay + " presses from revealing the easter egg.";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+
+                    eggDisplay--;
+                }
+            }
+
+            if (eggCounter == 6)
+            {
+                imageView.setImageResource(R.drawable.caesar_salad);
             }
         });
     }
