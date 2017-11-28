5958449fca6e952b5bddfd75be54d61e65c51866
Devin Mutlu <dmutlu@users.noreply.github.com>
2017-11-28 15:26:37 -0500

    Added Nick's animation code.
    Signed-off-by: dmutlu <devin.mutlu+github@gmail.com>

--- app/src/main/java/edu/umbc/dmutlu1/caesarcipher/TeamActivity.java
+++ app/src/main/java/edu/umbc/dmutlu1/caesarcipher/TeamActivity.java
@@ -2,6 +2,8 @@ package edu.umbc.dmutlu1.caesarcipher;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class TeamActivity extends AppCompatActivity
 {
@@ -11,6 +13,10 @@ public class TeamActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_team);
+
+        ImageView imageView = findViewById(R.id.imageTeam);
+        imageView.setOnClickListener(view ->
+                imageView.startAnimation(AnimationUtils.loadAnimation(this, R.anim.img_rotate)));
     }
 
     //Plays an exit animation when the user presses the back button.
